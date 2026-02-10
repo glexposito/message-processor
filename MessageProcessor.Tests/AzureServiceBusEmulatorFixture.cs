23e1255049d725c262c029dcd91b6e91d539504f
Guille <glexposito@gmail.com>
2026-02-10 20:33:21 +1300

    test: adjust service bus emulator startup for podman
    - use fixed port bindings
    - wait for emulator log readiness

--- MessageProcessor.Tests/AzureServiceBusEmulatorFixture.cs
+++ MessageProcessor.Tests/AzureServiceBusEmulatorFixture.cs
@@ -1,6 +1,7 @@
 using DotNet.Testcontainers.Builders;
 using Testcontainers.ServiceBus;
 using WireMock.Server;
+
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
 namespace MessageProcessor.Tests;
@@ -8,29 +9,26 @@ namespace MessageProcessor.Tests;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class AzureServiceBusEmulatorFixture : IAsyncLifetime
 {
-    private readonly ServiceBusContainer _serviceBusContainer;
     private const ushort ServiceBusPort = 5672;
     private const ushort ServiceBusHttpPort = 5300;
-    
-    public AzureServiceBusEmulatorFixture()
-    {
-        _serviceBusContainer = new ServiceBusBuilder()
-            .WithAcceptLicenseAgreement(true)
-            .WithPortBinding(ServiceBusPort, true)
-            .WithPortBinding(ServiceBusHttpPort, true)
-            .WithOutputConsumer(Consume.RedirectStdoutAndStderrToConsole())
-            // ReSharper disable once StringLiteralTypo
-            .WithName("azure-servicebus-emulator")
-            .WithLabel("purpose", "integration-test")
-            .WithWaitStrategy(
-                Wait.ForUnixContainer()
-                    .UntilHttpRequestIsSucceeded(req =>
-                        req.ForPort(ServiceBusHttpPort).ForPath("/health")))
-            .WithCleanUp(true)
-            .WithPrivileged(true)
-            .Build();
-    }
-    
+
+    private readonly ServiceBusContainer _serviceBusContainer = new ServiceBusBuilder()
+        .WithAcceptLicenseAgreement(true)
+        .WithPortBinding(ServiceBusPort, ServiceBusPort)
+        .WithPortBinding(ServiceBusHttpPort, ServiceBusHttpPort)
+        .WithOutputConsumer(Consume.RedirectStdoutAndStderrToConsole())
+        // ReSharper disable once StringLiteralTypo
+        .WithName("azure-servicebus-emulator")
+        .WithLabel("purpose", "integration-test")
+        .WithWaitStrategy(
+            Wait.ForUnixContainer()
+                .UntilMessageIsLogged(".*Emulator Service is Successfully Up!.*")
+        )
+        .WithCleanUp(true)
+        .WithPrivileged(true)
+        .Build();
+
+    // ReSharper disable once StringLiteralTypo
     public WireMockServer WireMockServer { get; private set; }
 
     public string ConnectionString => _serviceBusContainer.GetConnectionString();
